391b5c14f8aa507830001a7d0f96f27557ad93c5
Arnaud Gr <arnaud.grandval@gmail.com>
2018-09-12 19:48:16 +0200

    added wednesdays files

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+setTimeout("afficherMessage()", 3000);
+
+		function afficherMessage()
+		{
+			reponse = confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
+			if (reponse==true) {
+				document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+			}
+			
+		}
+
+		function changementApparence()
+		{
+			var elmt = document.getElementById("exemple");
+			elmt.style.backgroundColor = "magenta";
+
+			
+			var xx = document.getElementsByClassName("paragraphes");
+			xx.style.color = "blue";
+
+		}
